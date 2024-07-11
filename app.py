74de4b280be05fe27b7c157070b3b6e2ffb2f9e1
Aryan Tele <telearyan12@gmail.com>
2024-07-11 23:24:13 +0530

    convert text->chunks->vectors

--- app.py
+++ app.py
@@ -11,3 +11,28 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+# load pdf and extract text
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return text
+
+# convert text into chunks
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000,chunk_overlap=1000)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+#convert chunks into vectors for gemini prompt
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
+    vector_store.save_local("faiss_index")
